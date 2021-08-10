b9ce46af49dca18531ef32a65158c0a745fc029a
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2021-08-10 16:37:30 +0300

    fix linter errors

--- main.js
+++ main.js
@@ -65,33 +65,33 @@ class UI {
 addButton.addEventListener('click', (e) => {
   e.preventDefault();
   if (document.getElementById('title').value === '' && document.getElementById('author').value === '') {
-
-  } else {
-    const newBook = UI.createBook();
-    UI.saveBook(newBook);
-    UI.displayBook();
-    const books = UI.findBook();
-    if (books.length === 0) {
-      const book = UI.createBook();
-      const listItem = document.createElement('li');
-      listItem.innerHTML = `<h5>${book.title}</h5>
+    return;
+  }
+  const newBook = UI.createBook();
+  UI.saveBook(newBook);
+  UI.displayBook();
+  const books = UI.findBook();
+  if (books.length === 0) {
+    const book = UI.createBook();
+    const listItem = document.createElement('li');
+    const deleteBtn = document.createElement('button');
+    listItem.innerHTML = `<h5>${book.title}</h5>
     <h5>${book.author} </h5>`;
-      deleteBtn.id = book.title;
-      deleteBtn.className = 'removeBtn';
-      const br = document.createElement('hr');
-      list.appendChild(listItem);
-      listItem.appendChild(deleteBtn);
-      listItem.appendChild(br);
-      deleteBtn.addEventListener('click', () => {
-        if (deleteBtn.id === book.title) {
-          const index = addBooks.findIndex((rBook) => rBook.title === deleteBtn.id);
-          addBooks.splice(index, 1);
-          list.removeChild(listItem);
-          localStorage.setItem('books', JSON.stringify(books));
-        }
-      });
-      UI.saveBook(book);
-    }
+    deleteBtn.id = book.title;
+    deleteBtn.className = 'removeBtn';
+    const br = document.createElement('hr');
+    list.appendChild(listItem);
+    listItem.appendChild(deleteBtn);
+    listItem.appendChild(br);
+    deleteBtn.addEventListener('click', () => {
+      if (deleteBtn.id === book.title) {
+        const index = books.findIndex((rBook) => rBook.title === deleteBtn.id);
+        books.splice(index, 1);
+        list.removeChild(listItem);
+        localStorage.setItem('books', JSON.stringify(books));
+      }
+    });
+    UI.saveBook(book);
   }
 });
 
