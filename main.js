146d5dc7d6506bf21d2fcea90247d631cb951dde
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2021-08-10 13:07:15 +0300

    Add clearField method and display method to the UI class

--- main.js
+++ main.js
@@ -17,10 +17,9 @@ class UI {
 
   static saveBook(newBook) {
     const books = JSON.parse(localStorage.getItem('books'));
-    if(books === null) {
+    if (books === null) {
       localStorage.setItem('books', JSON.stringify([]));
-    }
-    else {
+    } else {
       books.push(newBook)
       localStorage.setItem('books', JSON.stringify(books));
     }
@@ -29,11 +28,47 @@ class UI {
   static findBook() {
     return JSON.parse(localStorage.getItem('books'));
   }
-}
+
+  static clearField() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+  }
+
+  static displayBook() {
+    const addBooks = UI.findBook() || '[]';
+    list.innerHTML = '';
+    addBooks.forEach((bookItem) => {
+      const listItem = document.createElement('li');
+      const deleteBtn = document.createElement('button');
+      deleteBtn.innerText = 'Remove';
+      listItem.innerHTML = `<h5>${bookItem.title}</h5>
+    <h5>${bookItem.author} </h5>`;
+      deleteBtn.id = bookItem.title;
+      deleteBtn.className = 'removeBtn';
+      const br = document.createElement('hr');
+      list.appendChild(listItem);
+      listItem.appendChild(deleteBtn);
+      listItem.appendChild(br);
+      deleteBtn.addEventListener('click', () => {
+        if (deleteBtn.id === bookItem.title) {
+          const index = addBooks.findIndex((rBook) => rBook.title === deleteBtn.id);
+          addBooks.splice(index, 1);
+          list.removeChild(listItem);
+          localStorage.setItem('books', JSON.stringify(addBooks));
+        }
+      });
+    });
+    UI.clearField();
+  }
+};
+
 const addNewBook = (title, author) => {
   const addBooks = JSON.parse(localStorage.getItem('books'));
   if (addBooks !== null) {
-    addBooks.push({ title: title.value, author: author.value });
+    addBooks.push({
+      title: title.value,
+      author: author.value
+    });
 
     bookList(addBooks);
     list.innerHTML = '';
@@ -58,7 +93,9 @@ const addNewBook = (title, author) => {
         }
       });
     });
-  } else { bookList(books); }
+  } else {
+    bookList(books);
+  }
 };
 
 addButton.addEventListener('click', () => {
